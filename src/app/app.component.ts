88b629fccbe64ca91e6d710bdb56bf88363ed4b4
Angel-Davila <a6519150005@utch.edu.mx>
2022-06-01 21:13:55 -0600

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { pipeline } from 'stream';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  public showNavbar = 1;
+
+  login() {
+    
+  }
 }
